bc32201e1fb5c9e36b8c749b543bccec54b9ce0a
Lily Johnston <lilyjohnston@Lilys-MacBook-Air-4.local>
2023-09-06 00:31:16 +1200

    Fully commented out program explaining what code does to improve the usability and future proofing

--- main.py
+++ main.py
@@ -193,11 +193,15 @@ def details(D):
     :param D: list (Customer details list)
     :return: None
     """
+    # when details are already stored within the details list, if statement will be used to ask user if they would like to re-enter details
     if len(D) != 0:
+        # short entry function is used to only allow a yes or no answer
         detail_correction = get_short_entry(
             "There are details already stored in our system, would you like to re-enter them? (Yes or No) --> ")
+        # if the user's details don't need to be re-entered, the user will return to the main function
         if detail_correction == "No":
             return None
+        # if the user's details do need to be re-entered, the current details will be cleared and the user will continue through the function
         elif detail_correction == "Yes":
             D.clear()
 
@@ -207,10 +211,12 @@ def details(D):
     D : Delivery
        '''
     print(collection_options)
+    # user is asked which collection method of collection they would like, this is done by the short_entry function as p and d are made to equal yes and no
     collection = get_short_entry("Please select your collection option: ", ['p', 'd']).upper()
 
     # if statement program will run if pickup is chosen
     if collection == "P":
+        # user is asked to enter applicable details, because pickup has been chosen their address is not applicable
         print("Please enter your customer details below:")
         print("-" * 80)
         name = get_string("Please enter your full name --> ")
@@ -218,24 +224,28 @@ def details(D):
         address = "Not Applicable"
         collection_method = "Pickup"
         print("-" * 80)
+        # details are formatted and then appended to the details list so that they are stored within the program
         details = [name, phone_number, collection_method, address]
         D.append(details)
 
     # elif statement program will run if delivery is chosen
     elif collection == "D":
+        # user is asked to enter applicable details, address is needed due to the collection method being delivery
         print("Please enter your customer details below:")
         print("-" * 80)
         name = get_string("Please enter your full name --> ")
         phone_number = get_phone_number("Please enter your phone number --> ")
         address = get_string("Please enter your address --> ")
+        # user is notified about the automatic delivery fee that will be added to their total when their order is finalized
         print("An automatic $3 delivery free will be added to your total")
         collection_method = "Delivery"
         print("-" * 80)
+        # details are formatted and then appended to the details list so that they are stored within the program
         details = [name, phone_number, collection_method, address]
         D.append(details)
 
 
-# review the order and customer details
+# function to review the order and customer details
 def review_order(C, D):
     """
 
@@ -243,38 +253,50 @@ def review_order(C, D):
     :param D: list (Customer details list)
     :return: None
     """
+    # this functionally mainly includes formatting to produce a receipt like piece of code showing the final order and details
     print("Marsden Gourmet Sandwich Bar Order:")
     print("-" * 150)
+    # headers for the order is formatted as index, amount , name, dietary, price, overall price
     order_title = "{:5}: {:6}: {:78} {:14} {:9} {:11}".format("Index", "Amount", "Sandwich Type", "Dietary", "Price",
                                                               "Total Price")
+    # printing the headers for the order list
     print(order_title)
     print("-" * 150)
     total = 0
     for i in range(0, len(C)):
-        # index, amount , name, dietary, price, overall price
+        # order list is formatted as index, amount , name, dietary, price, overall price
         output = "{:5}: {:6}: {:74} --- {:10} --- {:5} --- {:<4.2f}".format(i, C[i][0], C[i][1], C[i][2], C[i][3],
                                                                             C[i][4])
+        # the total of all sandwiches is calculated
         total += C[i][4]
+        # printing the order from the order list
         print(output)
     print("-" * 150)
     print("Details:")
     print("-" * 150)
+    # headers for the details is formatted as name, phone number, collection method, address
     delivery_title = "{:20} {:20} {:20} {:30}".format("Name", "Phone Number", "Collection Method", "Address")
+    # printing the headers for the details list
     print(delivery_title)
     for i in range(0, len(D)):
-        # name, phone number, collection method, address
+        # details list is formatted as name, phone number, collection method, address
         output = f"{D[i][0]:20} {D[i][1]:20} {D[i][2]:20} {D[i][3]:30}"
+        # printing the details from the details list
         print(output)
 
     print("-" * 150)
+    # the total price is being calculated
     amount_due = total
+    # if statement is used to define whether the collection method is delivery, if so the delivery fee is added
     if "Delivery" in range(2, len(D)):
         amount_due += 3
+    # total price is calculated and printed, rounded to two decimal places
     print("Total Price - {:.2f}".format(amount_due))
     print("-" * 150)
     return None
 
 
+# function is called upon to finalize the users order and start a new one when finalized
 def finalize_order(C, D):
     """
 
@@ -282,37 +304,47 @@ def finalize_order(C, D):
     :param D: list (Customer details list)
     :return: None
     """
+    # if the length of the customers order list is 0, they haven't ordered the finalization function will not run as there is nothing to finalise
     if len(C) == 0:
+        # the function will instead ask them to return to the main menu and place an order
         print(
             "You do not currently have any data entered within your order, please order before finalizing your order.")
         return None
 
+    # if the length of the customer details list is 0, they haven't entered details the finalization function will not run as there is no details to send the order too
     if len(D) == 0:
+        # the function will instead ask them to return to the main menu and enter their details
         print("You do not currently have any details entered within your order, please enter your details before "
               "finalizing your order.")
         return None
 
+    # function will print the final receipt using the review_order function
     print(review_order(C, D))
+    # they will be asked if they want to confirm their order using the short_entry (yes/no) function
     confirm_order = get_short_entry("Would you like to confirm your order? (Yes/No) -> ").upper()
+    # if they want to confirm their order both the customer details and customer order lists will be cleared, they will be notified a new order is starting and will be returned to the main function
     if confirm_order == "YES":
         print("Thank you for your order at Marsden's Gourmet Sandwich Bar")
         C.clear()
         D.clear()
         print("A new order is beginning . . .")
         print("-" * 150)
+    # if they do not wish to confirm their order they will be returned to the main function to continue their order
     elif confirm_order == "NO":
         print("-" * 150)
+    # any other inputs will be identified as an error and the user will be asked to re-enter an input
     else:
         print("Error: Yes No should have been captured on get short entry")
     return None
 
 
-# menu_list
+# funtion holding the menu_list
 def main():
     """Run program
 
     :return: None
     """
+    # sandwich list holding the names, dietary requirements and price of the sandwiches which is then used across almost every function
     my_sandwiches = [
         ["Halloumi and Apricot Jam", "Vegetarian", 15.95],
         ["Roasted Beetroot, Carrots, Spiced Nuts and Whipped Feta", "Vegetarian", 15.95],
@@ -322,10 +354,13 @@ def main():
         ["Buttermilk Chicken, Scotch Bonnet Jam, Pickled Cabbage and Crispy Shallots", "No Dietary", 18.95]
     ]
 
+    # order list which gains details of the order formatted by order_amount, name, dietary, price, overall_price
     customer_order = []
-    # order_amount, name, dietary, price, overall_price
+
+    # details list which gains details of the customer formatted by name, phone number, collection method, address
     customer_details = []
 
+    # user choice list which gives the user options which link to functions above
     user_choice = '''
     M : Sandwich Menu
     O : Order 
@@ -336,40 +371,43 @@ def main():
     Q : Quit 
     '''
 
-    # customer option menu
+    # customer options menu
     run = True
     while run:
         print(user_choice)
+        # the user is asked to select what they would like to out of the options below which are linked to functions
         choice = get_string("Please select your option: ", 1, 1).upper()
         print("-" * 150)
-        # print sandwich menu
+        # output when m is entered, will cause the sandwich menu to print calling on the my_sandwiches list
         if choice == "M":
             print_sandwiches(my_sandwiches)
 
-        # order sandwiches
+        # output when o is entered, will cause the add sandwich function to run, calling on the menu and details list
         elif choice == "O":
             add_sandwich(my_sandwiches, customer_order)
 
-        # edit order
+        # output when e is entered, will cause the edit order function to run, calling on the order list
         elif choice == "E":
             edit_order(customer_order)
 
-        # enter customer details
+        # output when c is entered, will cause the details function to run, calling on the details list
         elif choice == "C":
             details(customer_details)
 
-        # review order
+        # output when r is entered, will cause the review order function to run, calling on the order and details list
         elif choice == "R":
             review_order(customer_order, customer_details)
 
+        # output when f is entered, will cause the finalize order function to run, calling on the order and details list
         elif choice == "F":
             finalize_order(customer_order, customer_details)
 
-        # quit the program
+        # output when q is entered, will cause the program to quit
         elif choice == "Q":
             print("Thank you")
             run = False
 
+        # output for all unexpected inputs
         else:
             print("Unrecognised entry")
 
